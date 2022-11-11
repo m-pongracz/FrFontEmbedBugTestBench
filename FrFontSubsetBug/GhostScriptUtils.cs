293079b03fc3c14c47c1d7bda2d0ac9747f732f0
Martin Pongrácz <martin@pongra.cz>
2022-11-11 14:50:46 +0100

    changed the project to test skia multithread bug scenario

---
+++ FrFontSubsetBug/GhostScriptUtils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FrFontSubsetBug;
+
+public class GhostScriptUtils
+{
+    private readonly string _outputPath;
+    private const string GhostScriptPathEnvVarName = "GHOSTSCRIPT_PATH";
+
+    private readonly string? _gsPath;
+    private static ReadOnlySpan<string> PdfToPclArgs => new[]
+    {
+            "-q", // Quiet startup: suppress normal startup messages
+            "-dSAFER", // Enables access controls on files
+            "-dBATCH", // Causes Ghostscript to exit after processing all files
+            "-dNOPAUSE", // Disables the prompt and pause at the end of each page
+        };
+
+    public GhostScriptUtils(string outputPath)
+    {
+        _outputPath = outputPath;
+        _gsPath = Environment.GetEnvironmentVariable(GhostScriptPathEnvVarName);
+    }
+
+    public async Task<string> ConvertPdfAsync(FileInfo pdfFile, string outputDevice, int dpiResolution)
+    {
+        if (_gsPath == null)
+        {
+            throw new ArgumentNullException(
+                $"{GhostScriptPathEnvVarName} environment variable has not been set. Set its value to a path leading to the ghoscript executable.");
+        }
+
+        var fileName = pdfFile.Name.Split('.')[0];
+
+        var outputFile = Path.Combine(_outputPath, $"{fileName}.pcl");
+
+        var logPath = Path.Combine(_outputPath, "gs_log.txt");
+        
+        var args = new List<string>(PdfToPclArgs.ToArray())
+            {
+                $"-sstdout={logPath}",
+                $"-sDEVICE={outputDevice}",
+                $"-r{dpiResolution}",
+                @"-sOutputFile=" + outputFile,
+                @"-f",
+                pdfFile.FullName
+            };
+
+        using var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo(_gsPath, string.Join(' ', args))
+        };
+
+        process.Start();
+        
+        await process.WaitForExitAsync();
+
+        var logContents = await File.ReadAllTextAsync(logPath);
+
+        return logContents;
+    }
+}
---
+++ FrFontSubsetBug/README.md
@@ -0,0 +1,12 @@
+# FrFontSubsetBug
+
+This project tests various situations that break fonts in fast reports
+
+Inspect `Main` method to see what run configurations are available.
+
+To run this project, you must put the following packages in the `lib` folder
+
+- FastReport.Compat.Skia.2022.3.2.nupkg
+- FastReport.Core.Skia.2022.3.12.nupkg
+- FastReport.DataVisualization.Skia.2022.3.2.nupkg
+- FastReport.SkiaDrawing.2022.3.4.nupkg
